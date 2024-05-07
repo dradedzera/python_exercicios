d11f571e0ab42dd9b98c04f6eb39f0c74967332f
dradedzera <gustavodeandrade9090@gmail.com>
2024-05-07 10:08:35 -0300

    exercicio1.py

---
+++ exercicio_1.py
@@ -0,0 +1,10 @@
+# Solicita que o usuário insira dois números inteiros
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+
+# Calcula a soma dos dois números
+soma = num1 + num2
+
+# Calcula a soma dos dois números
+print("A soma dos dois números é:", soma)
+print(f"A multiplicação dos produtos é : {num1*num2}")
